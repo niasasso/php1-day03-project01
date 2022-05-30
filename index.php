afa81e612fa518c895c612ee7926ee0881f0dec6
Sulenias Asso <74882893+niasasso@users.noreply.github.com>
2022-05-30 19:41:49 +0900

    Simpan pertama

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+  $judul = 'SaCode - PHP Batch I Day 03';
+  $isi = 'Belajar PHP untuk pemula di SaCode.';
+ ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $judul; ?></title>
+  </head>
+  <body>
+
+    <h1><?php echo $judul; ?></h1>
+    <p><?php echo $isi; ?></p>
+
+  </body>
+</html>
